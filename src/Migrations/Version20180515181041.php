5abbb1bd0cfbb42e5322c7d598faef63a491da92
root <anonhq789@protonmail.com>
2018-06-20 16:08:06 +0200

    :rocket: Migrations

--- src/Migrations/Version20180515181041.php
+++ src/Migrations/Version20180515181041.php
@@ -21,8 +21,8 @@ final class Version20180515181041 extends AbstractMigration
         $this->addSql('CREATE TABLE symfony_demo_post_tag (post_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_6ABC1CC44B89032C (post_id), INDEX IDX_6ABC1CC4BAD26311 (tag_id), PRIMARY KEY(post_id, tag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE Ptr6zJ_tag (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_54B65485E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE kf7bnJ_comment ADD CONSTRAINT FK_D5CDE9D04B89032C FOREIGN KEY (post_id) REFERENCES fk1TmS_post (id)');
-        $this->addSql('ALTER TABLE kf7bnJ_comment ADD CONSTRAINT FK_D5CDE9D0F675F31B FOREIGN KEY (author_id) REFERENCES s2FpUk_user (id)');
-        $this->addSql('ALTER TABLE fk1TmS_post ADD CONSTRAINT FK_B331874AF675F31B FOREIGN KEY (author_id) REFERENCES s2FpUk_user (id)');
+        $this->addSql('ALTER TABLE kf7bnJ_comment ADD CONSTRAINT FK_D5CDE9D0F675F31B FOREIGN KEY (author_id) REFERENCES h8WmKo_user (id)');
+        $this->addSql('ALTER TABLE fk1TmS_post ADD CONSTRAINT FK_B331874AF675F31B FOREIGN KEY (author_id) REFERENCES h8WmKo_user (id)');
         $this->addSql('ALTER TABLE symfony_demo_post_tag ADD CONSTRAINT FK_6ABC1CC44B89032C FOREIGN KEY (post_id) REFERENCES fk1TmS_post (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE symfony_demo_post_tag ADD CONSTRAINT FK_6ABC1CC4BAD26311 FOREIGN KEY (tag_id) REFERENCES Ptr6zJ_tag (id) ON DELETE CASCADE');
     }
@@ -37,7 +37,7 @@ final class Version20180515181041 extends AbstractMigration
         $this->addSql('ALTER TABLE kf7bnJ_comment DROP FOREIGN KEY FK_D5CDE9D04B89032C');
         $this->addSql('ALTER TABLE symfony_demo_post_tag DROP FOREIGN KEY FK_6ABC1CC44B89032C');
         $this->addSql('ALTER TABLE symfony_demo_post_tag DROP FOREIGN KEY FK_6ABC1CC4BAD26311');
-        $this->addSql('DROP TABLE s2FpUk_user');
+        $this->addSql('DROP TABLE h8WmKo_user');
         $this->addSql('DROP TABLE kf7bnJ_comment');
         $this->addSql('DROP TABLE fk1TmS_post');
         $this->addSql('DROP TABLE symfony_demo_post_tag');
