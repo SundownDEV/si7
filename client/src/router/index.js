fa97d420fb8f4eceff311c8eef8d80e915d1b222
ClementGuerin <ticlem27270@gmail.com>
2018-06-25 16:45:15 +0200

    add admin panel

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+// Components
+
 import Homepage from '@/components/Homepage'
 import Login from '@/components/Login'
 import Register from '@/components/Register'
 import Error404 from '@/components/Error404'
+
+import AdminHomepage from '@/components/admin/AdminHomepage'
 
 Vue.use(Router)
 
@@ -24,6 +29,21 @@ export default new Router({
       path: '/register',
       name: 'Register',
       component: Register
+    },
+    {
+      path: '/admin',
+      redirect: '/admin/homepage',
+      name: 'Admin',
+      component: {
+        render (c) { return c('router-view') }
+      },
+      children: [
+        {
+          path: 'homepage',
+          name: 'AdminHomepage',
+          component: AdminHomepage
+        }
+      ]
     },
     {
       path: '*',
