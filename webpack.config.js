e5c36f8038f069c9a17468239eee55c680f55a2f
sundowndev <raphael@crvx.fr>
2018-06-27 16:25:12 +0200

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,18 @@ Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
-    .autoProvidejQuery()
-    .autoProvideVariables({
-        "window.Bloodhound": require.resolve('bloodhound-js'),
-        "jQuery.tagsinput": "bootstrap-tagsinput"
-    })
+    //.autoProvidejQuery()
     .enableSassLoader()
     .enableVersioning(false)
-    .createSharedEntry('js/common', ['jquery'])
-    .addEntry('js/app', './assets/js/app.js')
+    //.createSharedEntry('js/common', ['jquery'])
+    /*.addEntry('js/app', './assets/js/app.js')
     .addEntry('js/login', './assets/js/login.js')
     .addEntry('js/admin', './assets/js/admin.js')
     .addEntry('js/search', './assets/js/search.js')
     .addStyleEntry('css/app', ['./assets/scss/app.scss'])
     .addStyleEntry('css/signin', ['./assets/scss/signin.scss'])
-    .addStyleEntry('css/admin', ['./assets/scss/admin.scss'])
+    .addStyleEntry('css/admin', ['./assets/scss/admin.scss'])*/
+    .addStyleEntry('css/common', ['./assets/scss/common.scss'])
 ;
 
 module.exports = Encore.getWebpackConfig();
