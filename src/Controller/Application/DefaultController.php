f00489e810ec7aae03f57f36a0b7e1c0c179c86d
sundowndev <raphael@crvx.fr>
2018-06-27 16:24:51 +0200

    Controllers and templates

--- src/Controller/Application/DefaultController.php
+++ src/Controller/Application/DefaultController.php
@@ -11,9 +11,6 @@
 
 namespace App\Controller\Application;
 
-use App\Entity\Post;
-use App\Form\PostType;
-use App\Repository\PostRepository;
 use App\Utils\Slugger;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,38 +20,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Controller used to manage blog contents in the backend.
- *
- * Please note that the application backend is developed manually for learning
- * purposes. However, in your real Symfony application you should use any of the
- * existing bundles that let you generate ready-to-use backends without effort.
- *
- * See http://knpbundles.com/keyword/admin
- *
- * @Route("/admin/post")
- * @Security("has_role('ROLE_ADMIN')")
- *
- * @author Ryan Weaver <weaverryan@gmail.com>
- * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ * @Route("/app", name="app_")
+ * @Security("has_role('ROLE_USER') or has_role('ROLE_ADMIN')")
  */
 class DefaultController extends AbstractController
 {
     /**
-     * Lists all Post entities.
+     * Dashboard index.
      *
-     * This controller responds to two different routes with the same URL:
-     *   * 'admin_post_index' is the route with a name that follows the same
-     *     structure as the rest of the controllers of this class.
-     *   * 'admin_index' is a nice shortcut to the backend homepage. This allows
-     *     to create simpler links in the templates. Moreover, in the future we
-     *     could move this annotation to any other controller while maintaining
-     *     the route name and therefore, without breaking any existing link.
-     *
-     * @Route("/", methods={"GET"}, name="admin_index")
-     * @Route("/", methods={"GET"}, name="admin_post_index")
+     * @Route("/", methods={"GET"}, name="index")
      */
     public function index(): Response
     {
-        return $this->render('admin/blog/index.html.twig');
+        return $this->render('app/index.html.twig');
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,28 +11,68 @@
 
 namespace App\Controller;
 
+use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+use App\Form\UserType;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 /**
  * Controller used to manage the application security.
- * See https://symfony.com/doc/current/cookbook/security/form_login_setup.html.
  *
- * @author Ryan Weaver <weaverryan@gmail.com>
- * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ * @Route("/", name="security_")
  */
 class SecurityController extends AbstractController
 {
+
     /**
-     * @Route("/login", name="security_login")
+     * @Route("/register", methods={"GET","POST"}, name="signup")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('app_index');
+        }
+
+        return $this->render(
+            'default/security_signup.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+    /**
+     * @Route("/login", name="login")
      */
     public function login(AuthenticationUtils $helper): Response
     {
-        return $this->render('security/login.html.twig', [
+        return $this->render('default/security_login.html.twig', [
             // last username entered by the user (if any)
-            'last_username' => $helper->getLastUsername(),
+            'last_email' => $helper->getLastUsername(),
             // last authentication error (if any)
             'error' => $helper->getLastAuthenticationError(),
         ]);
@@ -44,7 +84,7 @@ class SecurityController extends AbstractController
      * But, this will never be executed. Symfony will intercept this first
      * and handle the logout automatically. See logout in config/packages/security.yaml
      *
-     * @Route("/logout", name="security_logout")
+     * @Route("/logout", name="ogout")
      */
     public function logout(): void
     {
